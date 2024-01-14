8c19ef1db1eb85df981da10294ee9a07bd59a6d1
KaiLin Chang <kalin5850@gmail.com>
2024-01-14 10:16:32 -0500

    add application properties

--- src/main/java/com/pykaicode/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/pykaicode/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,13 +1,23 @@
 package com.pykaicode.springboot.demo.mycoolapp.rest;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class FunRestController {
+
+  @Value("${coach.name}")
+  private String coachName;
+
   @GetMapping("/")
   public String sayHello() {
     return "Hello World !!";
+  }
+
+  @GetMapping("/applicationprops")
+  public String printApplicationProperties() {
+    return this.coachName;
   }
 
 }
