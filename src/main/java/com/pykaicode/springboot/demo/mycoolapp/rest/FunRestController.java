0b4d6d69b13fdeb1b264319b946a58246ba24e9a
KaiLin Chang <kalin5850@gmail.com>
2024-01-14 10:06:01 -0500

    add path and controller

---
+++ src/main/java/com/pykaicode/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,13 @@
+package com.pykaicode.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class FunRestController {
+  @GetMapping("/")
+  public String sayHello() {
+    return "Hello World !!";
+  }
+
+}
