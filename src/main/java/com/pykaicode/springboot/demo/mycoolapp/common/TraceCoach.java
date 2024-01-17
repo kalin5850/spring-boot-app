f85d69120679d6aeada5f9618b501a7b544d49e2
KaiLin Chang <kalin5850@gmail.com>
2024-01-16 19:02:15 -0500

    the dedault scope of bean is singleton, and can be changed by using Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE

--- src/main/java/com/pykaicode/springboot/demo/mycoolapp/common/TraceCoach.java
+++ src/main/java/com/pykaicode/springboot/demo/mycoolapp/common/TraceCoach.java
@@ -1,10 +1,11 @@
 package com.pykaicode.springboot.demo.mycoolapp.common;
 
-import org.springframework.context.annotation.Lazy;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Lazy
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class TraceCoach implements Coach {
 
   public TraceCoach() {
--- src/main/java/com/pykaicode/springboot/demo/mycoolapp/rest/CoachRestController.java
+++ src/main/java/com/pykaicode/springboot/demo/mycoolapp/rest/CoachRestController.java
@@ -7,20 +7,31 @@ import org.springframework.web.bind.annotation.RestController;
 import com.pykaicode.springboot.demo.mycoolapp.common.Coach;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class CoachRestController {
   private Coach myCoach;
+  private Coach anotherCoach;
 
   @Autowired
-  public CoachRestController(@Qualifier("traceCoach") Coach myCoach) {
+  public CoachRestController(
+      @Qualifier("traceCoach") Coach myCoach,
+      @Qualifier("traceCoach") Coach anotherCoach) {
     System.err.println("In constructor " + getClass().getSimpleName());
     this.myCoach = myCoach;
+    this.anotherCoach = anotherCoach;
   }
 
   @GetMapping("/dailyworkout")
   public String getDailyWorkout() {
     return myCoach.getDailyWorkout();
+  }
+
+  @GetMapping("/check")
+  public String check() {
+    return "Comparing beans: myCoach == anotherCoach, " +
+        (this.myCoach == this.anotherCoach);
   }
 
 }
